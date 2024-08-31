40d5d622213750f9c3c068c9343af48e4b65dafe
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2024-08-31 06:09:35 +0530

    made the changes in the Home.jsx

--- proj_beaches/src/components/Footer.jsx
+++ proj_beaches/src/components/Footer.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full z-50 bg-gradient-to-r from-emerald-400 to-blue-500 shadow-lg">
-      <div className="max-w-screen-xl mx-auto px-4">
-        <nav className="flex justify-around items-center h-16">
+    <footer className="fixed bottom-0 flex items-center w-full left-0  h-[8vh] z-50 bg-gradient-to-r from-emerald-400 to-blue-500 shadow-lg">
+    
+        <div className="flex justify-around items-center w-full h-full ">
           <Link to='/search'  ><FooterButton icon={<Search size={24} />} label="Search" /></Link>
           <Link to='/map' ><FooterButton icon={<Map size={28} />} label="Map" /></Link>
           <Link to='/BookMark' ><FooterButton icon={<Bookmark size={24} />} label="Bookmark" /></Link>
-        </nav>
-      </div>
+        </div>
+
     </footer>
   )
 }
